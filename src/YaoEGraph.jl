70ccd6597da4838f43cc47cf70dc25a1c14e69fb
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-09 23:39:53 -0400

    some initial tests

--- src/YaoEGraph.jl
+++ src/YaoEGraph.jl
@@ -1,5 +1,22 @@
 module YaoEGraph
 
-# Write your package code here.
+using Metatheory
+using Metatheory.EGraphs
+@metatheory_init
+
+# Custom type APIs for the GATExpr
+using YaoHIR
+using Metatheory.TermInterface
+
+TermInterface.gethead(t::Chain) = :call
+TermInterface.getargs(t::Chain) = [:chain, t.args...]
+TermInterface.gethead(t::Gate) = :call
+TermInterface.getargs(t::Gate) = [:gate, t.locations]
+TermInterface.gethead(t::Ctrl) = :call
+TermInterface.getargs(t::Ctrl) = [:ctrl, t.gate, t.ctrl]
+
+function EGraphs.extractnode(::EGraph, n::ENode{Chain}, extractor::Function)
+    return Chain(extractor.(n.args[2:end])...)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using YaoEGraph
 using Test
 
-@testset "YaoEGraph.jl" begin
-    # Write your tests here.
+using Metatheory, Metatheory.EGraphs
+using YaoHIR
+using YaoLocations
+using YaoHIR: X
+
+@metatheory_init
+
+naive = @theory begin
+    Chain(Gate(X, locs), Gate(X, locs)) |> Chain([])
 end
+
+circ = Chain(Gate(X, Locations(1)), Gate(X, Locations(1)))
+egraph = EGraph(circ)
+saturate!(egraph, naive)
+extract!(egraph, astsize)
