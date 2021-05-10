559c81fe6842572bfcd23afb5db4560388204501
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-10 19:29:29 -0400

    fix gate & ctrl

--- src/YaoEGraph.jl
+++ src/YaoEGraph.jl
@@ -14,7 +14,7 @@ TermInterface.arity(t::Chain) = length(TermInterface.getargs(t))
 TermInterface.istree(e::Chain) = true
 
 TermInterface.gethead(t::Gate) = :call
-TermInterface.getargs(t::Gate) = [:gate, t.locations]
+TermInterface.getargs(t::Gate) = [:gate, t.operation,  t.locations]
 TermInterface.arity(t::Gate) = length(TermInterface.getargs(t))
 TermInterface.istree(e::Gate) = true
 
@@ -24,16 +24,16 @@ TermInterface.arity(t::Ctrl) = length(TermInterface.getargs(t))
 TermInterface.istree(e::Ctrl) = true
 
 
-function EGraphs.extractnode(g::EGraph, n::ENode{Chain}, extractor::Function)
+function EGraphs.extractnode(::EGraph, n::ENode{Chain}, extractor::Function)
     return Chain(extractor.(n.args[2:end])...)
 end
 
-# function EGraphs.extractnode(g::EGraph, n::ENode{Gate}, extractor::Function)
-#     return Gate(extractor.(n.args[2:end])...)
-# end
+function EGraphs.extractnode(::EGraph, n::ENode{Gate}, extractor::Function)
+    return Gate(extractor(n.args[2]), extractor(n.args[3]))
+end
 
-# function EGraphs.extractnode(g::EGraph, n::ENode{Ctrl}, extractor::Function)
-#     return Ctrl(extractor.(n.args[2:end])...)
-# end
+function EGraphs.extractnode(::EGraph, n::ENode{Ctrl}, extractor::Function)
+    return Ctrl(extractor(n.args[2]), extractor(n.args[3]))
+end
 
 end
